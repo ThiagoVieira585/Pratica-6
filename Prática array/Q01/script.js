dadd59f7fd48838213adf5610fbd470d49abeb4f
Thiago Vieira <60758912+ThiagoVieira585@users.noreply.github.com>
2022-11-10 21:51:47 -0300

    Add files via upload

---
+++ Prática array/Q01/script.js
@@ -0,0 +1,4 @@
+var arr = [1,2,3,4];
+
+var arr2 = arr.map(x => Math.pow(x,2));
+document.write(arr2);
---
+++ Prática array/Q02/script.js
@@ -0,0 +1,9 @@
+let arr = [-1, 2, 4, 5]
+function MaiorQueZero(value){
+    if(value>0){
+        return value;
+    }
+}
+
+let arr2 = arr.filter(MaiorQueZero);
+document.write(arr2)
